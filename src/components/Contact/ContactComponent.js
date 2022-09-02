1b0672e8bc02774fe92fb96362d737e85a281505
Flame221 <43928831+Flame221@users.noreply.github.com>
2022-09-02 16:00:25 +0200

    Flexible for phone size.

--- src/components/Contact/ContactComponent.js
+++ src/components/Contact/ContactComponent.js
@@ -1,17 +1,19 @@
-import { Heading, HStack, Stack } from '@chakra-ui/react'
+import { Heading, Stack, useMediaQuery } from '@chakra-ui/react'
 import React from 'react'
 import ContactForm from '../ContactForm'
 import MyLinks from './MyLinks'
 
 const ContactComponent = () => {
+  const [isLargerScreen] = useMediaQuery('(min-width: 800px)');
+
   return (
     <Stack direction='column' align={'center'} spacing={{ base: 8, md: 14 }} py={{ base: 10, md: 10 }}>
       <Heading>Contact Me</Heading>
-      <HStack spacing={{ base: 8, md: 14 }}>
+      <Stack direction={isLargerScreen ? 'row' : 'column'} align={'center'} spacing={{ base: 8, md: 14 }}>
         <ContactForm/>
         <Heading>OR</Heading>
         <MyLinks/>
-      </HStack>
+      </Stack>
     </Stack>
   )
 }
